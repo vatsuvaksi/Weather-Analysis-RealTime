611d9f876980157bc181f2c92008666006856dce
vatsal gupta <62776100+vatsuvaksi@users.noreply.github.com>
2023-09-10 12:32:04 -0700

    Added Network Structs

---
+++ models/network/weatherData/realTimeData.go
@@ -0,0 +1,43 @@
+package weatherdata
+
+type RealTimeData struct {
+	Location struct {
+		Name           string  `json:"name"`
+		Region         string  `json:"region"`
+		Country        string  `json:"country"`
+		Lat            float64 `json:"lat"`
+		Lon            float64 `json:"lon"`
+		TzID           string  `json:"tz_id"`
+		LocaltimeEpoch int64   `json:"localtime_epoch"`
+		Localtime      string  `json:"localtime"`
+	} `json:"location"`
+	Current struct {
+		LastUpdatedEpoch int64   `json:"last_updated_epoch"`
+		LastUpdated      string  `json:"last_updated"`
+		TempC            int     `json:"temp_c"`
+		TempF            float64 `json:"temp_f"`
+		IsDay            int     `json:"is_day"`
+		Condition        struct {
+			Text string `json:"text"`
+			Icon string `json:"icon"`
+			Code int    `json:"code"`
+		} `json:"condition"`
+		WindMph    float64 `json:"wind_mph"`
+		WindKph    float64 `json:"wind_kph"`
+		WindDegree int     `json:"wind_degree"`
+		WindDir    string  `json:"wind_dir"`
+		PressureMb int     `json:"pressure_mb"`
+		PressureIn float64 `json:"pressure_in"`
+		PrecipMm   int     `json:"precip_mm"`
+		PrecipIn   float64 `json:"precip_in"`
+		Humidity   int     `json:"humidity"`
+		Cloud      int     `json:"cloud"`
+		FeelslikeC float64 `json:"feelslike_c"`
+		FeelslikeF float64 `json:"feelslike_f"`
+		VisKm      int     `json:"vis_km"`
+		VisMiles   int     `json:"vis_miles"`
+		UV         int     `json:"uv"`
+		GustMph    float64 `json:"gust_mph"`
+		GustKph    float64 `json:"gust_kph"`
+	} `json:"current"`
+}
