0fff4cc3f69385586d829b4db0ad5fa8ac8df7df
vatsal gupta <62776100+vatsuvaksi@users.noreply.github.com>
2023-09-10 11:43:49 -0700

    Added getFuture API

--- weatherclient/apis/getFuture.go
+++ weatherclient/apis/getFuture.go
@@ -7,30 +7,29 @@ import (
 )
 
 /*
-Function Calls the weather API to get Forecast using the paramater q  which can either be
+Function Calls the weather API to get real time Data using the paramater q which can either be
 lattitude,longitude value in the format "lat,lang" or it can be a single zip code "zipCode"
-and days in string format
 */
-func GetForecast(q, days string) ([]byte, error) {
+func GetFuture(q, dt string) ([]byte, error) {
 	var apiKey = os.Getenv("API_KEY")
-	fmt.Println("Initiated API call for GetForecast")
+	fmt.Println("Initiated API call for GetFuture")
 	weatherClientResource, err := weatherclient.GetWeatherClientResource()
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
 	} else {
-		var url = "forecast.json"
+		var url = "future.json"
 		response, err := weatherClientResource.GetDataFromClient(url, map[string]string{
-			"q":    q,
-			"key":  apiKey,
-			"days": days,
+			"q":   q,
+			"key": apiKey,
+			"dt":  dt,
 		})
 
 		if err != nil {
 			fmt.Println(err)
 			return nil, err
 		} else {
-			fmt.Println("GetForecast Data Generated response")
+			fmt.Println("GetFuture Data Generated response")
 			return response, nil
 		}
 	}
