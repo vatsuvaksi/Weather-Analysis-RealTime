c82ec495eb5e7f6aaa257e5318b4884e19e3a29b
vatsal gupta <62776100+vatsuvaksi@users.noreply.github.com>
2023-09-10 12:03:15 -0700

    removed comments main

--- main.go
+++ main.go
@@ -3,12 +3,4 @@ package main
 func main() {
 	InitializeWeatherApplication()
 
-	// //Testing once
-	// resp, err := apis.GetRealTimeData("48.8567,2.3508")
-	// if err != nil {
-	// 	panic(err)
-	// } else {
-	// 	fmt.Println("here")
-	// 	fmt.Println(string(resp.Body()))
-	// }
 }
