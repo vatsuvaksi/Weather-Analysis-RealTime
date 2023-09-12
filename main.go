3a6f62d299435cff5d6b21568b6c7d9d2c565408
vatsal gupta <62776100+vatsuvaksi@users.noreply.github.com>
2023-09-12 11:27:39 -0700

    modified main

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-// "fmt"
-// weatherdata "real-time-weather-app/models/network/weatherData"
-// "real-time-weather-app/weatherclient/apis"
-
-// Find a way to run this concurrently
 func main() {
 	initializeWeatherApplication()
-	// var channelsToHold = make(chan weatherdata.RealTimeData, 2)
-	// // go func() {
-	// // 	channelsToHold ,err <- apis.GetRealTimeData("48.8567,2.3508")
-	// // }()
-	// // go func() {
-	// // 	channelsToHold ,err <- apis.GetRealTimeData("48.8567,2.3508")
-	// // }()
-	// // resp, err := apis.GetRealTimeData("48.8567,2.3508")
-
 }
