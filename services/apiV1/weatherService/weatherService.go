dba492207dfd0b357029913938a5c027a56eac10
vatsal gupta <62776100+vatsuvaksi@users.noreply.github.com>
2023-10-08 11:36:15 -0700

    Started Swagger Docuumentation process

--- services/apiV1/weatherService/weatherService.go
+++ services/apiV1/weatherService/weatherService.go
@@ -13,6 +13,18 @@ func NewWeatherService() *WeatherService {
 	return &WeatherService{}
 }
 
+// @Summary Get real-time weather data
+// @Description Retrieve real-time weather data based on the provided query.
+// @Tags Weather
+// @Accept json
+// @Produce json
+// @Param q query string true "Location query"
+// @Success 200 {object} RealTimeData "Successful response"
+// @Failure 400 {object} ErrorResponse "Bad request"
+// @Failure 401 {object} ErrorResponse "Unauthorized"
+// @Failure 404 {object} ErrorResponse "Not found"
+// @Failure 500 {object} ErrorResponse "Internal server error"
+// @Router /weather/real-time [get]
 func (ws *WeatherService) GetRealTimeData(q string) (*weatherData.RealTimeData, error) {
 	// Calling Internal Services to Get RealTime Data
 	response, err := apis.GetRealTimeData(q)
@@ -21,6 +33,20 @@ func (ws *WeatherService) GetRealTimeData(q string) (*weatherData.RealTimeData, 
 	}
 	return response, nil
 }
+
+// @Summary Get weather forecast data
+// @Description Retrieve weather forecast data based on the provided query and number of days.
+// @Tags Weather
+// @Accept json
+// @Produce json
+// @Param q query string true "Location query"
+// @Param days query string true "Number of days for the forecast"
+// @Success 200 {object} ForeCastData "Successful response"
+// @Failure 400 {object} ErrorResponse "Bad request"
+// @Failure 401 {object} ErrorResponse "Unauthorized"
+// @Failure 404 {object} ErrorResponse "Not found"
+// @Failure 500 {object} ErrorResponse "Internal server error"
+// @Router /weather/forecast [get]
 func (ws *WeatherService) GetForecast(q, days string) (*weatherData.ForeCastData, error) {
 	// Calling Internal Services to Get Forecast Data
 	response, err := apis.GetForecast(q, days)
@@ -29,6 +55,20 @@ func (ws *WeatherService) GetForecast(q, days string) (*weatherData.ForeCastData
 	}
 	return response, nil
 }
+
+// @Summary Get future weather data
+// @Description Retrieve future weather data based on the provided query and date.
+// @Tags Weather
+// @Accept json
+// @Produce json
+// @Param q query string true "Location query"
+// @Param dt query string true "Date for the forecast (YYYY-MM-DD format)"
+// @Success 200 {object} FutureData "Successful response"
+// @Failure 400 {object} ErrorResponse "Bad request"
+// @Failure 401 {object} ErrorResponse "Unauthorized"
+// @Failure 404 {object} ErrorResponse "Not found"
+// @Failure 500 {object} ErrorResponse "Internal server error"
+// @Router /weather/future [get]
 func (ws *WeatherService) GetFuture(q, dt string) (*weatherData.FutureData, error) {
 	// Calling Internal Services to Get Future Data
 	response, err := apis.GetFuture(q, dt)
@@ -37,6 +77,19 @@ func (ws *WeatherService) GetFuture(q, dt string) (*weatherData.FutureData, erro
 	}
 	return response, nil
 }
+
+// @Summary Get time zone data
+// @Description Retrieve time zone data based on the provided location query.
+// @Tags Weather
+// @Accept json
+// @Produce json
+// @Param q query string true "Location query"
+// @Success 200 {object} TimeZoneData "Successful response"
+// @Failure 400 {object} ErrorResponse "Bad request"
+// @Failure 401 {object} ErrorResponse "Unauthorized"
+// @Failure 404 {object} ErrorResponse "Not found"
+// @Failure 500 {object} ErrorResponse "Internal server error"
+// @Router /weather/time-zone [get]
 func (ws *WeatherService) GetTimeZone(q string) (*weatherData.TimeZoneData, error) {
 	// Calling Internal Services to GetTimeZone Data
 	response, err := apis.GetTimeZone(q)
