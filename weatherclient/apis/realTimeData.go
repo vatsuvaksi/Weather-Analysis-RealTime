0441ce4e300a2b3038070ba8f367e6ae45db3dde
vatsal gupta <62776100+vatsuvaksi@users.noreply.github.com>
2023-10-29 12:06:51 -0700

    Added concurrency for service realtime data

--- weatherclient/apis/realTimeData.go
+++ weatherclient/apis/realTimeData.go
@@ -10,19 +10,31 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type ConcurrentFetchData struct {
+	Data *weatherdata.RealTimeData
+	Err  error
+}
+
 /*
 Function Calls the weather API to get real time Data using the paramater q which can either be
 lattitude,longitude value in the format "lat,lang" or it can be a single zip code "zipCode"
 */
 func GetRealTimeData(q string) (*weatherdata.RealTimeData, error) {
-	var apiKey = os.Getenv("API_KEY")
-	var realTimeData *weatherdata.RealTimeData
-	loggers.Logger.Info("Initiated API call for GetRealTimeData")
-	weatherClientResource, err := weatherclient.GetWeatherClientResource()
-	if err != nil {
-		loggers.Logger.Info(err)
-		return realTimeData, err
-	} else {
+
+	// Create a channel for results
+	resultCh := make(chan ConcurrentFetchData)
+
+	// Launch a goroutine to fetch data
+	go func() {
+		var apiKey = os.Getenv("API_KEY")
+		var realTimeData *weatherdata.RealTimeData
+		loggers.Logger.Info("Initiated API call for GetRealTimeData")
+		weatherClientResource, err := weatherclient.GetWeatherClientResource()
+		if err != nil {
+			loggers.Logger.Info(err)
+			resultCh <- ConcurrentFetchData{Data: nil, Err: err}
+			return
+		}
 		var url = "current.json"
 		response, err := weatherClientResource.GetDataFromClient(url, map[string]string{
 			"q":   q,
@@ -34,18 +46,21 @@ func GetRealTimeData(q string) (*weatherdata.RealTimeData, error) {
 				"Status":  500,
 				"isFatal": false,
 			}).Warn("Error Fetching Info from client for realTimeData API ")
-			return realTimeData, err
-		} else {
-			if err := json.Unmarshal(response, &realTimeData); err != nil {
-				loggers.Logger.WithField("Key", logrus.Fields{
-					"Status":  500,
-					"isFatal": false,
-				}).Warn("Error Unmarshalling RealTime Data ")
-				return realTimeData, err
-			} else {
-				loggers.Logger.Info("Get Real Time Data Generated response")
-				return realTimeData, nil
-			}
+			resultCh <- ConcurrentFetchData{Data: nil, Err: err}
+			return
 		}
-	}
+		if err := json.Unmarshal(response, &realTimeData); err != nil {
+			loggers.Logger.WithField("Key", logrus.Fields{
+				"Status":  500,
+				"isFatal": false,
+			}).Warn("Error Unmarshalling RealTime Data ")
+			resultCh <- ConcurrentFetchData{Data: nil, Err: err}
+			return
+		}
+		loggers.Logger.Info("Get Real Time Data Generated response")
+		resultCh <- ConcurrentFetchData{Data: realTimeData, Err: nil}
+	}()
+	// Wait for the result from the goroutine
+	result := <-resultCh
+	return result.Data, result.Err
 }
